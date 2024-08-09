244c9106b1fc8ef195e8bc20fba15877eb5f65a6
Isaac Silva <isak.silva81@gmail.com>
2024-08-08 22:56:25 -0500

    finished

---
+++ EmployeeSQL/table_schemata_emp.sql
@@ -0,0 +1,33 @@
+--Data Engineering
+-- Table Schemas
+CREATE TABLE titles (
+    title_id VARCHAR(10) PRIMARY KEY,
+    title VARCHAR(50) NOT NULL
+);
+CREATE TABLE departments (
+    dept_no VARCHAR(10) PRIMARY KEY,
+    dept_name VARCHAR(50) NOT NULL
+);
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR(10) REFERENCES titles(title_id),
+    birth_date DATE,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    sex CHAR(1),
+    hire_date DATE
+);
+CREATE TABLE salaries (
+    emp_no INT PRIMARY KEY REFERENCES employees(emp_no),
+    salary INT
+);
+CREATE TABLE dept_emp (
+    emp_no INT REFERENCES employees(emp_no),
+    dept_no VARCHAR(10) REFERENCES departments(dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(10) REFERENCES departments(dept_no),
+    emp_no INT REFERENCES employees(emp_no),
+    PRIMARY KEY (dept_no, emp_no)
+);
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sql-challenge
+This repository has the following folders:
+	data:
+		Provided Data
+	EmployeeSQL:
+		ERD Diagram
+		queries_emp.sql
+		table_schemata_emp.sql
